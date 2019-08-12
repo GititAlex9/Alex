c3990b2a69fd3412509d7ad25c6e6e9bf405a70b
GititAlex9 <54040057+GititAlex9@users.noreply.github.com>
2019-08-12 15:21:22 -0400

    Add files via upload

---
+++ Play, writing code. practice temp Day 1 extra.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 12 11:25:42 2019
+
+@author: columbia
+"""
+
+print("hello world, 10 divided by 5 equals") #welcome, information on divison
+print(10/5)
+print("Answer")
+print("I am learning to code, 100 divided by 3 is")
+print(100/3)
+
+print("Grand Answer isss.............. 5 times 4 is")
+print(5*4)
+
+print("5+4=")
+print(5+4)
+
+print("5-1=")
+print(5-1)
+
+print(5**2) #squaring
+print((5*6)/3)
+
+def add(a,b):
+    add = a+b
+    print(add)
+add(5555.5,105555)
+
+def adds(a,b,c):
+    adds = a+b+c
+    print(adds)
+    print("The sum of ",a,",",b,"and",c,"='s")
+adds(10,5,5)
+
+def multiply(a,b):
+    multiply = a*b
+    print(multiply)
+multiply(5,5)
+    
+
+#def NameOfFunction(parameters)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if (5+4) is (9) 
+#print("correct")
+#path = '9'
+#if path:('9')
+#    print "True"
+#else:
+ #   print "False"
+    
